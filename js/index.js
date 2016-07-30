4b83005b35e4ce1e737aee86e20c37ecadce9819
wybiral <davy.wybiral@gmail.com>
2016-07-30 09:52:44 -0500

    Setup three.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,42 @@
 class App {
     constructor() {
-        // Do something here
+        // Grab window properties
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+        let pixelRatio = window.devicePixelRatio;
+        let aspect = width / height;
+        // Setup three.js
+        this.camera = new THREE.PerspectiveCamera(45, aspect, 0.5, 1500);
+        this.scene = new THREE.Scene();
+        this.renderer = new THREE.WebGLRenderer({antialias: false});
+        this.renderer.setPixelRatio(pixelRatio);
+        this.renderer.setSize(width, height);
+        document.body.appendChild(this.renderer.domElement);
+    }
+
+    /* Start the main loop */
+    start() {
+        this.loop();
+    }
+
+    loop() {
+        requestAnimationFrame(() => this.loop());
+        this.update();
+        this.render();
+    }
+
+    update() {
+    }
+
+    render() {
+        let scene = this.scene;
+        let camera = this.camera;
+        let renderer = this.renderer;
+        renderer.render(scene, camera);
     }
 }
 
 window.onload = function() {
     let app = new App();
+    app.start();
 };
